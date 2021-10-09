5135702a3807cdb0fd8ec57ef12577997b77ab74
VolodyaZAVR <chuguy.x.vladimir@gmail.com>
2021-10-10 02:51:01 +0300

    Add a simple string in program //testing git//

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -3,4 +3,5 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
+    println("Hello World!")
 }
