81103aa8a31b6ceb63661f4177fb56a42dfe6cf2
VolodyaZAVR <chuguy.x.vladimir@gmail.com>
2021-10-10 02:54:25 +0300

    Add a cycle in program //testing git//

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -4,4 +4,7 @@ fun main(args: Array<String>) {
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
     println("Hello World!")
+    for(i in 0..10){
+        println(i)
+    }
 }
