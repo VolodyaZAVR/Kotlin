7fe3971bf1593715a4320c63b3161e7f5fb8bb52
BOYSDONTCRY <62401307+VolodyaZAVR@users.noreply.github.com>
2021-10-11 15:59:02 +0300

    Revert "Lab1 realization"

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -53,8 +53,4 @@ fun main() {
     val text = "Text for format"
     val alText = AlignedText()
     println(alText.alignText(text, 140, AlignedText.Alignment.LEFT))
-    for (i in 0..10)
-        println(i)
-    for (i in 0..10)
-        println(i)
 }
