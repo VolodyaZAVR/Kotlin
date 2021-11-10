e0a276be7bbe48e4cf05e4a9df9a03870de3aec5
VolodyaZAVR <chuguy.x.vladimir@gmail.com>
2021-11-10 12:44:39 +0300

    first version of left alight implication

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -12,37 +12,75 @@ class AlignedText {
         alignment: Alignment = Alignment.LEFT
     ): String {
         return when (alignment) {
-            Alignment.LEFT -> alightTextLift(text, lineWidth)
+            Alignment.LEFT -> alightTextLeft(text, lineWidth)
             Alignment.RIGHT -> alightTextRight(text, lineWidth)
             Alignment.CENTER -> alightTextCenter(text, lineWidth)
             Alignment.JUSTIFY -> alightTextJustify(text, lineWidth)
         }
     }
 
-    private fun alightTextLift(
+    private fun alightTextLeft(
         text: String,
-        lineWidth: Int = 120
+        lineWidth: Int
     ): String {
-        return "not realized"
+        if (text.isBlank()) return ""
+        var resultText: String = ""
+        var currLength: Int = 0
+        val listValues: List<String> = text.split(" ").map { it.trim() }
+        for (item in listValues) {
+            if ((item.length + currLength) > lineWidth) {
+                if (item.length > lineWidth) {
+                    resultText = resultText.plus("\n")
+                    currLength = 0
+                    val charArr: CharArray = item.toCharArray()
+                    for (c in charArr) {
+                        if (currLength < lineWidth) {
+                            resultText = resultText.plus(c)
+                            currLength += 1
+                        } else {
+                            resultText = resultText.plus("\n")
+                            resultText = resultText.plus(c)
+                            currLength = 1
+                        }
+                    }
+                } else {
+                    resultText = resultText.plus("\n")
+                    resultText = resultText.plus(item)
+                    currLength = item.length
+                }
+            } else {
+                resultText = resultText.plus(item)
+                currLength += item.length
+            }
+            if (currLength == lineWidth) {
+                resultText = resultText.plus("S")
+                resultText = resultText.plus("\n")
+                currLength = 1
+            } else {
+                resultText = resultText.plus("S")
+                currLength += 1
+            }
+        }
+        return resultText
     }
 
     private fun alightTextRight(
         text: String,
-        lineWidth: Int = 120
+        lineWidth: Int
     ): String {
         return "not realized"
     }
 
     private fun alightTextCenter(
         text: String,
-        lineWidth: Int = 120
+        lineWidth: Int
     ): String {
         return "not realized"
     }
 
     private fun alightTextJustify(
         text: String,
-        lineWidth: Int = 120
+        lineWidth: Int
     ): String {
         return "not realized"
     }
@@ -50,11 +88,7 @@ class AlignedText {
 
 
 fun main() {
-    val text = "Text for format"
+    val text = "..... ...... ............ ................../"
     val alText = AlignedText()
-    println(alText.alignText(text, 140, AlignedText.Alignment.LEFT))
-    for (i in 0..10)
-        println(i)
-    for (i in 0..10)
-        println(i)
+    println(alText.alignText(text, 10, AlignedText.Alignment.LEFT))
 }
