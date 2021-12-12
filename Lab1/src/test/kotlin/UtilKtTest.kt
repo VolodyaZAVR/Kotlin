1ef5b3edcdbc83bce824031dda62ba9c603937f5
Sherieva9302 <58978082+Sherieva9302@users.noreply.github.com>
2021-12-12 17:45:21 +0300

    fixed test for lab1

--- Lab1/src/test/kotlin/UtilKtTest.kt
+++ Lab1/src/test/kotlin/UtilKtTest.kt
@@ -5,28 +5,65 @@ import kotlin.test.assertFailsWith
 class UtilKtTest {
 
     @Test
-    fun `left align test1`() { //проверка устраняемости лишних пробелов в начале и конце
-        val text = " Текст  "
-        val alText = AlignedText()
-        val res = alText.alignText(text, 20, AlignedText.Alignment.LEFT)
-        assertEquals(res, "Текст")
-    }
-    @Test
-    fun `left align test2`() { //проверка устраняемости лишних пробелов между словами
-        val text = "Текст  о      чем-то"
+    fun `left align checking for an empty string`() {
+        val text = ""
         val alText = AlignedText()
         val res = alText.alignText(text, 40, AlignedText.Alignment.LEFT)
-        assertEquals(res, "Текст о чем-то")
+        assertEquals(
+            res, ""
+        )
     }
 
     @Test
-    fun `left align test3`() { //проверка сохраняемости переносов
-        val text = "Над нашей страной вьются тучи\nИ головы мёрзнут у нас\nИ если один купит шляпу\nМиллионы воскликнут: «Как раз!»"
+    fun `left align checking for the absence of new symbols`() {
+        val text = "Текст"
         val alText = AlignedText()
-        val res = alText.alignText(text, 150, AlignedText.Alignment.LEFT)
-        assertEquals(res, "Над нашей страной вьются тучи\n" +
-                "И головы мёрзнут у нас\n" +
-                "И если один купит шляпу\n" +
-                "Миллионы воскликнут: «Как раз!»")
+        val res = alText.alignText(text, 40, AlignedText.Alignment.LEFT)
+        assertEquals(
+            res, "Текст"
+        )
     }
+
+
+    @Test
+    fun `left align checking for the absence of new characters`() {
+        val text =
+            "Над нашей страной вьются тучи\nИ головы мёрзнут у нас\nИ если один купит шляпу\nМиллионы воскликнут: «Как раз!»"
+        val alText = AlignedText()
+        val res = alText.alignText(text, 40, AlignedText.Alignment.LEFT)
+        assertEquals(
+            res, "Над нашей страной вьются тучи\n" +
+                    "И головы мёрзнут у нас\n" +
+                    "И если один купит шляпу\n" +
+                    "Миллионы воскликнут: «Как раз!»"
+        )
+    }
+
+    @Test
+    fun `left align checking for the absence of new characters after line breaks`() {
+        val text =
+            "Над нашей страной вьются тучи\nИ головы мёрзнут у нас\nИ если один купит шляпу\nМиллионы воскликнут: «Как раз!»"
+        val alText = AlignedText()
+        val res = alText.alignText(text, 25, AlignedText.Alignment.LEFT)
+        assertEquals(
+            res, "Над нашей страной вьются \n" +
+                    "тучи\n" +
+                    "И головы мёрзнут у нас\n" +
+                    "И если один купит шляпу\n" +
+                    "Миллионы воскликнут: \n" +
+                    "«Как раз!»"
+        )
+    }
+
+    @Test
+    fun `left align checking for simple line breaks`() {
+        val text = "Декабрь - лучшее время для сдачи первой лабораторной"
+        val alText = AlignedText()
+        val res = alText.alignText(text, 20, AlignedText.Alignment.LEFT)
+        assertEquals(
+            res, "Декабрь - лучшее \n" + "время для сдачи \n" + "первой лабораторной"
+        )
+    }
+
 }
+
