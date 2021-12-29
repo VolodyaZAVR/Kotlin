94fe33098be0c9296fd25a37b422dba95f755386
VolodyaZAVR <chuguy.x.vladimir@gmail.com>
2021-12-29 10:28:30 +0300

    add Lab6

--- Lab2/src/main/kotlin/calc/Parser.kt
+++ Lab2/src/main/kotlin/calc/Parser.kt
@@ -1,11 +1,26 @@
 package calc
 
 class Parser {
-    fun isCorrectExpression(input : String) : Boolean{
+    private val tokens: List<Char> = listOf(
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '+', '-', '*', '/', '^'
+    )
+
+    fun isCorrectExpression(input: String): Boolean {
         val expr = input.replace(" ", "")
+        var bracketsCount = 0
         if (expr.isBlank())
             throw IllegalArgumentException("Incorrect expression!")
-        // TODO("доделать проверку выражения на корректность")
+        for (item in expr) {
+            if (!tokens.contains(item)) {
+                throw IllegalArgumentException("The expression contains an invalid character.")
+            }
+            if (item == '(')
+                bracketsCount++
+            if (item == ')' && bracketsCount == 0)
+                throw IllegalArgumentException("Incorrect expression!")
+            if (item == ')')
+                bracketsCount--
+        }
         return true
     }
 }
---
+++ Lab6/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import shapeCollector.Comparators
+import shapeCollector.ShapeCollector
+import shapeCollector.ShapeFactoryImpl
+import shapeCollector.Shape
+import shapeCollector.Circle
+
+fun main() {
+    val factory = ShapeFactoryImpl()
+    val shapeCollector = ShapeCollector<Shape>()
+    val circleCollector = ShapeCollector<Circle>()
+
+    circleCollector.add(factory.createCircle(10.0))
+    circleCollector.add(factory.createCircle(20.0))
+    circleCollector.add(factory.createRandomCircle())
+    circleCollector.add(factory.createRandomCircle())
+    circleCollector.add(factory.createRandomCircle())
+
+    println(circleCollector.getAllSorted(Comparators.sortCircleRadiusDesc))
+    println("\n")
+
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+    shapeCollector.add(factory.createRandomShape())
+
+    println(shapeCollector.getAllSorted(Comparators.sortPerimeter))
+    println("\n")
+    println(shapeCollector.getAllSorted(Comparators.sortAreaDesc))
+}
